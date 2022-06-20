e8b457e2efba8560c26cbc2a352a9c8a580aae97
Ilya Goroshchik <goooroshchik@gmail.com>
2022-06-20 17:58:23 +0300

    Add config file

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
