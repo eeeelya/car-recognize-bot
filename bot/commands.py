f80f5d5592223c7960e8b9b42385787ba890fd89
Ilya Goroshchik <goooroshchik@gmail.com>
2022-06-21 16:01:52 +0300

    Delete commands which worked with db

--- bot/commands.py
+++ bot/commands.py
@@ -1,35 +1,38 @@
+import boto3
+
 from aiogram.types import BotCommand, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters import Text
+from bot.config import S3_KEY, S3_SECRET_KEY, BUCKET_NAME
 
 
 async def set_commands(bot):
     commands = [
-        BotCommand(command="/start", description="Мой профиль"),
+        BotCommand(command="/menu", description="Menu"),
     ]
     await bot.set_my_commands(commands)
 
 
 async def start(message):
-    hi_message = "Welcome to the Сar Recognition Bot. You can choose what interests you:"
+    hi_message = "Welcome to the Сar Recognition Bot."
 
+    await message.answer(hi_message)
+    await menu(message)
+
+
+async def menu(message):
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton(text="Classification", callback_data="classification"))
-    keyboard.add(InlineKeyboardButton(text="All launches", callback_data="all_launches"))
 
-    await message.answer(hi_message, reply_markup=keyboard)
+    await message.answer(text="You can choose what interests you:", reply_markup=keyboard)
 
 
 async def classification(call):
     await call.message.answer("start classification")
-    await call.answer()
 
-
-async def all_launches(call):
-    await call.message.answer("all_launches")
     await call.answer()
 
 
 async def register_commands(dp):
     dp.register_message_handler(start, commands=["start"], state="*")
+    dp.register_message_handler(menu, commands=["menu"], state="*")
     dp.register_callback_query_handler(classification, Text(equals="classification"), state="*")
-    dp.register_callback_query_handler(all_launches, Text(equals="all_launches"), state="*")
