a4fde9fda290df52c6a188b9f7ee6baf513d104b
Ilya Goroshchik <goooroshchik@gmail.com>
2022-06-21 15:57:03 +0300

    Update bot launch without db

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import asyncio
 import logging
-import asyncpg
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher
 from bot import config
 from bot.commands import set_commands, register_commands
 
@@ -14,20 +13,7 @@ async def main():
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher(bot)
 
-    pool = await asyncpg.create_pool(
-        user=config.POSTGRES_USER,
-        password=config.POSTGRES_PASSWORD,
-        host=config.POSTGRES_HOST,
-        database=config.POSTGRES_DB,
-        command_timeout=60,
-    )
-
-    # await create_tables(pool)
-
     await set_commands(bot)
-
-    # dp.middleware.setup(DbMiddleware(pool))
-    # dp.middleware.setup(ThrottlingMiddleware())
 
     await register_commands(dp)
 
