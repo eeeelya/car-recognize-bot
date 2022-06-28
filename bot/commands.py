53758101dea95d2be7480bc5c705ac433ca9d5ac
Ilya Goroshchik <goooroshchik@gmail.com>
2022-06-28 14:14:19 +0300

    Update command get_photo

--- bot/commands.py
+++ bot/commands.py
@@ -1,8 +1,8 @@
-import boto3
-
 from aiogram.types import BotCommand, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters import Text
-from bot.config import S3_KEY, S3_SECRET_KEY, BUCKET_NAME
+
+from bot.state import LaunchState
+from model.utils import read_image, launch_calculations, get_results
 
 
 async def set_commands(bot):
@@ -27,12 +27,32 @@ async def menu(message):
 
 
 async def classification(call):
-    await call.message.answer("start classification")
+    await call.message.answer("Start classification.")
+    await call.message.answer("Please, upload your image: ")
+    await LaunchState.image.set()
 
     await call.answer()
+
+
+async def get_photo(message, state):
+    await message.answer("Image was uploaded.")
+    await state.update_data(image=message.photo[-1].file_id)
+
+    response = await message.bot.download_file_by_id(message.photo[-1].file_id)
+
+    image = await read_image(response)
+
+    await message.answer("Inference start.")
+    res = await launch_calculations(image)
+
+    text = await get_results(res)
+    await message.answer(text)
+
+    await state.finish()
 
 
 async def register_commands(dp):
     dp.register_message_handler(start, commands=["start"], state="*")
     dp.register_message_handler(menu, commands=["menu"], state="*")
     dp.register_callback_query_handler(classification, Text(equals="classification"), state="*")
+    dp.register_message_handler(get_photo, content_types=['photo'], state=LaunchState.image)
